8116504021fa9a48d6e358da3c7fa4bb7b397307
vakagayatri <vakagayatrireddy@gmail.com>
2020-12-19 23:40:48 -0500

    server changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose")
 const nameModel = require("./budget_schema")
 const userModel = require('./user_schema')
